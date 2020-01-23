3e33bae484e41139bb84c0c5c269140c67c246a1
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-01-23 14:11:25 +0300

    sales table to class

--- src/components/sales-table/sales-table.js
+++ src/components/sales-table/sales-table.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import './sales-table.css';
 
-const SalesTable = ({ items }) => {
-  const renderRow = (item, idx) => {
+class SalesTable extends Component {
+  //= ({ items }) => {
+
+  renderRow = (item, idx) => {
     const { id, product, count, sum, date, client, manager, filename } = item;
     return (
       <tr key={id}>
@@ -20,26 +22,29 @@ const SalesTable = ({ items }) => {
     );
   };
 
-  return (
-    <div className='sales-table-block'>
-      <table className='table'>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Product</th>
-            <th>Count</th>
-            <th>Sum</th>
-            <th>Date</th>
-            <th>Client</th>
-            <th>Manager</th>
-            <th>File name</th>
-          </tr>
-        </thead>
-        <tbody>{items.map(renderRow)}</tbody>
-      </table>
-    </div>
-  );
-};
+  render() {
+    const { items } = this.props;
+    return (
+      <div className='sales-table-block'>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product</th>
+              <th>Count</th>
+              <th>Sum</th>
+              <th>Date</th>
+              <th>Client</th>
+              <th>Manager</th>
+              <th>File name</th>
+            </tr>
+          </thead>
+          <tbody>{items.map(this.renderRow)}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = ({ sales }) => {
   return {
